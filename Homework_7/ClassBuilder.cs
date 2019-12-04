92cd9756d96f89012d3276a5fc834459bf49b6ab
Oleg <OlegBilozor08@gmail.com>
2019-12-04 11:23:35 +0200

    Добавьте файлы проекта.

---
+++ Homework_7/ClassBuilder.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Homework_7
+{
+    public class ClassBuilder<T>
+    {
+        public void BuildClass()
+        {
+            var type = typeof(T);
+            string fileName = $"{type.Name}.cs";
+          
+            if (File.Exists(fileName))
+            {
+                Console.WriteLine("File with that name already exists. You want to create another one (enter 1) or replace the old one (enter 2)?");
+                int.TryParse(Console.ReadLine(), out int choise);
+                switch (choise)
+                {
+                    case 1:
+                        int num = 1;
+                        
+                        while (File.Exists(fileName))
+                        {
+                            fileName = $"{type.Name}_{num++}.cs";
+                        }
+
+                        Console.WriteLine($"Class will be written to file \"{fileName}\"");
+                        break;
+                    case 2:
+                        Console.WriteLine($"File \"{fileName}\" will be replaced with the new one");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice!");
+                        return;
+                }
+            }
+
+            using (FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.Write))
+            {
+                using (StreamWriter sw = new StreamWriter(fs))
+                {
+                    sw.WriteLine($"class {type.Name}\n{{");
+                    var properties = type.GetProperties();
+                    foreach (var property in properties)
+                    {
+                        string newLine = $"{property.PropertyType} {property.Name}\n{{\n";
+                        var get = property.GetMethod;
+                        if (get.IsPrivate)
+                        {
+                            newLine += "private ";
+                        }
+                        else if(get.IsPublic)
+                        {
+                            newLine += "public ";
+                        }
+                        else
+                        {
+                            newLine += "protected ";
+                        }
+                        newLine += "get;\n";
+                        var set = property.SetMethod;
+                        if (set.IsPrivate)
+                        {
+                            newLine += "private ";
+                        }
+                        else if (set.IsPublic)
+                        {
+                            newLine += "public ";
+                        }
+                        else
+                        {
+                            newLine += "protected ";
+                        }
+
+                        newLine += "set;\n}";
+                        sw.WriteLine(newLine);
+                    }
+                    sw.WriteLine("}");
+                }
+            }
+        }
+    }
+}
---
+++ Homework_7/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassBuilder<A> cb1= new ClassBuilder<A>();
+            ClassBuilder<B> cb2 = new ClassBuilder<B>();
+            cb1.BuildClass();
+            cb2.BuildClass();
+            cb1.BuildClass();
+            Console.ReadLine();
+        }
+    }
+
+    class A
+    {
+        public string Name { get; set; }
+        public int Age { get; private set; }
+
+    }
+
+    class B
+    {
+        public string Name { get; set; }
+        public string Address { get; protected set; }
+        public int Age { get; private set; }
+    }
+}
